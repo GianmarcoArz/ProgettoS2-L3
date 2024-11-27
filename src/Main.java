9347cccd6359ed8ea1d05db23d90bbec94fcc2f3
Arzanese Gianmarco <gianmarcoarzanese@gmail.com>
2024-11-27 15:57:48 +0100

    commitFinale

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,79 @@
+import TestDiOggi.Customer;
+import TestDiOggi.Order;
+import TestDiOggi.Prodotti;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+
+        Prodotti p1 = new Prodotti(9L,"IlSignoreDegliAnnelli","books",35.50);
+        Prodotti p2 = new Prodotti(7L,"IlGladiatore","books",20.50);
+        Prodotti p3 = new Prodotti(4L,"L'arteDellaGuerra","books",110.50);
+        Prodotti p4 = new Prodotti(2L,"Pistola","baby",0.50);
+        Prodotti p5 = new Prodotti(3L,"bazzuca","arma",1000);
+        Prodotti p6 = new Prodotti(8L,"mitra","arma",500);
+
+
+        List<Prodotti> books = new ArrayList<>();
+        books.add(p1);
+        books.add(p2);
+        books.add(p3);
+        books.add(p4);
+
+
+
+        List<Prodotti> booksPrice = books.stream()
+                .filter(prodotti -> prodotti.getCategory().equals("books") && prodotti.getPrice() > 100)
+                .toList();
+
+        System.out.println(booksPrice.toString());
+
+
+        Customer Riccardo = new Customer(1L,"Riccardo", 1);
+
+
+        Order o1 = new Order(11L,"spedito", LocalDate.now(),LocalDate.now(),books,Riccardo);
+        Order o2 = new Order(12L,"disperso", LocalDate.now(),LocalDate.now(),books,Riccardo);
+
+        List<Order> ordersBaby = new ArrayList<>();
+        ordersBaby.add(o1);
+        ordersBaby.add(o2);
+
+        List<Order> babyOrd = ordersBaby.stream()
+                .filter(order -> order.getProducts().stream().anyMatch(prodotti -> prodotti.getCategory().equals("baby")))
+                .collect(Collectors.toList());
+
+
+        babyOrd.forEach(System.out::println);
+
+
+        List<Prodotti> armi = new ArrayList<>();
+        armi.add(p5);
+        armi.add(p6);
+
+        List<Prodotti> armiBianche = armi.stream()
+                .filter(prodotti -> prodotti.getCategory().equals("arma"))
+                .map(prodotti -> new Prodotti(
+                        prodotti.getId(),
+                        prodotti.getName(),
+                        prodotti.getCategory(),
+                        prodotti.getPrice() * 0.90
+                ))
+                .collect(Collectors.toList());
+
+        armiBianche.forEach(prodotti -> System.out.println(prodotti.toString()));
+
+
+
+
+
     }
 }
---
+++ src/TestDiOggi/Prodotti.java
@@ -0,0 +1,57 @@
+package TestDiOggi;
+
+public class Prodotti {
+    private Long id;
+    private String name;
+    private String category;
+    private double price;
+
+    public Prodotti(Long id, String name, String category, double price) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Prodotti{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
